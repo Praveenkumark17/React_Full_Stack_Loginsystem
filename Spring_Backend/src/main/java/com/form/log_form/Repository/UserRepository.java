a92d0520c11173be6c36e9e9f735add5b5ee3a52
Praveenkumar K <praveenkrishh01@gmail.com>
2024-01-22 11:09:30 +0530

    remove backend files

--- Spring_Backend/src/main/java/com/form/log_form/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.form.log_form.Repository;
-
-import com.form.log_form.Model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    public User findByUserid(String data);
-}
