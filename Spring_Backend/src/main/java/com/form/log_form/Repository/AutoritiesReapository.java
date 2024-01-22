492cea60428b527a4d45d0ef7e5e127def436956
Praveenkumar K <praveenkrishh01@gmail.com>
2024-01-22 10:59:11 +0530

    remove backend files

--- Spring_Backend/src/main/java/com/form/log_form/Repository/AutoritiesReapository.java
+++
@@ -1,7 +0,0 @@
-package com.form.log_form.Repository;
-
-import com.form.log_form.Model.Authorities;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AutoritiesReapository extends JpaRepository<Authorities,Long> {
-}
